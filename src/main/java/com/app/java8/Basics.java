9f1fe0e71273674aa7c75d32f08bf13cbd2d5923
Chathuranga <chathuwithana@gmail.com>
2017-07-22 00:17:56 +0800

    Commit to master branch

--- src/main/java/com/app/java8/Basics.java
+++ src/main/java/com/app/java8/Basics.java
@@ -4,6 +4,11 @@ package com.app.java8;
  * @author Chathuranga Withana
  */
 public class Basics {
+    @Override
+    public String toString() {
+        return "Basics toString method";
+    }
+
     public static void main(String[] args) {
         System.out.println("Basics Class Goes Here...");
     }
