9195970e3d2af3af7c543c55ccb7e88c4156b427
Chathuranga <chathuwithana@gmail.com>
2017-07-22 00:13:59 +0800

    Feature 2 added commit 2

--- src/main/java/com/app/java8/test/feature2/FeatureTwo.java
+++ src/main/java/com/app/java8/test/feature2/FeatureTwo.java
@@ -1,4 +1,22 @@
 package com.app.java8.test.feature2;
 
 public class FeatureTwo {
+    private int no;
+    private String name;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
