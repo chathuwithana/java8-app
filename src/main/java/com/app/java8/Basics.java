742913d90e21ad6d3bbcd7200e345054cf213efa
Chathuranga <chathuwithana@gmail.com>
2017-07-21 23:53:43 +0800

    Add Basics class

---
+++ src/main/java/com/app/java8/Basics.java
@@ -0,0 +1,10 @@
+package com.app.java8;
+
+/**
+ * @author Chathuranga Withana
+ */
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Basics Class Goes Here...");
+    }
+}
